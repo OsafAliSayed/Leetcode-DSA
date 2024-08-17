7ed7aeb3722f4e13143ccd812fae2745fb071763
OsafAliSayed <99737087+OsafAliSayed@users.noreply.github.com>
2024-08-17 23:06:46 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (5.16%) - LeetSync

--- 165-compare-version-numbers/compare-version-numbers.cpp
+++ 165-compare-version-numbers/compare-version-numbers.cpp
@@ -1,38 +1,29 @@
 class Solution {
 public:
+    int getVersion(string& version) {
+        int val = 0;
+        if (version.length() != 0) {
+            int i = 0;
+            while (i < version.length() && version[i] != '.') {
+                i++;
+            }
+            val = stoi(version.substr(0, i));
+            if (i != version.length()) {
+                version = version.substr(i + 1);
+            }
+            else {
+                version = "";
+            }
+        }
+        return val;
+    }
     int compareVersion(string version1, string version2) {
         int i = 0;
         int j = 0;
         while (true) {
-            cout << version1 << " " << version2 << endl;
-            int val1 = 0;
-            int val2 = 0;
-            if (version1.length() != 0) {
-                int i = 0;
-                while (i < version1.length() && version1[i] != '.') {
-                    i++;
-                }
-                val1 = stoi(version1.substr(0, i));
-                if (i != version1.length()) {
-                    version1 = version1.substr(i + 1);
-                }
-                else {
-                    version1 = "";
-                }
-            }
-            if (version2.length() != 0) {
-                int i = 0;
-                while (i < version2.length() && version2[i] != '.') {
-                    i++;
-                }
-                val2 = stoi(version2.substr(0, i));
-                if (i != version2.length()) {
-                    version2 = version2.substr(i + 1);
-                }
-                else {
-                    version2 = "";
-                }
-            }
+            int val1 = getVersion(version1);
+            int val2 = getVersion(version2);
+            
             if (val1 < val2) {
                 return -1;
             }
