b61fe0866631908d4b82399eb92ff4d58d30baab
OsafAliSayed <99737087+OsafAliSayed@users.noreply.github.com>
2024-08-18 17:45:07 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (18.28%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,27 +1,34 @@
 class Solution {
 public:
+    vector<int> zMatch(string s) {
+        vector<int> z(s.size(), 0);
+        int l = 0, r = 0;
+        for (int i = 1; i < s.size(); i++) {
+            if (i < r) {
+                z[i] = z[i - l];
+
+                if (i + z[i] > r) {
+                    z[i] = r - i;
+                }
+            }
+    
+            while (i + z[i] < s.size() && s[z[i]] == s[i + z[i]]) z[i]++;
+
+            if (i + z[i] > r) {
+                l = i;
+                r = i + z[i];
+            }
+        }
+        return z;
+    }
     int strStr(string haystack, string needle) {
-        for (int i = 0; i < haystack.length(); i++) {
-            if (needle.length() > haystack.length() - i) {
-                return -1;
-            }
-            if (needle[0] == haystack[i]) {
-                int j = 0;
-                while (j < needle.length()) {
-                    if (needle[j] == haystack[i]) {
-                        i++;
-                        j++;
-                    }
-                    else {
-                        i = i - j;
-                        j = 0;
-                        break;
-                    }
-                }
-                if (j == needle.length()) {
-                    return i - j;
-                }
-            }
+        string temp = needle + "$" + haystack;
+        vector<int> z = zMatch(temp);
+        for (auto itr: z) {
+            cout << itr << " ";
+        }
+        for (int i = needle.length() + 1; i < z.size(); i++) {
+            if (z[i] == needle.length()) return i - needle.length() - 1;
         }
         return -1;
     }
